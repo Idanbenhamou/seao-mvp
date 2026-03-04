8e0e95549b9f3ec97be7d87b2cec804495c7cd4a
Idanbenhamou <idan_benhamou@icloud.com>
2026-03-04 15:16:10 +0200

    Update RoleCard.jsx

--- app/ui/RoleCard.jsx
+++ app/ui/RoleCard.jsx
@@ -1,24 +1,16 @@
 import Link from "next/link";
 
-type RoleCardProps = {
-  title: string;
-  desc: string;
-  imageUrl: string;
-  href: string;
-};
-
-export function RoleCard({ title, desc, imageUrl, href }: RoleCardProps) {
+export function RoleCard({ title, desc, imageUrl, href }) {
   return (
-    <div className="roleCard" style={{}} aria-label={title}>
+    <div className="roleCard" data-title={title}>
       <style>{`
         .roleCard[data-title="${title}"]::before{ background-image:url('${imageUrl}'); }
       `}</style>
-      <div className="roleCard" data-title={title}>
-        <div className="roleInner">
-          <div className="roleTitle">{title}</div>
-          <div className="roleDesc">{desc}</div>
-          <Link className="btn roleBtn" href={href}>Continue</Link>
-        </div>
+
+      <div className="roleInner">
+        <div className="roleTitle">{title}</div>
+        <div className="roleDesc">{desc}</div>
+        <Link className="btn roleBtn" href={href}>Continue</Link>
       </div>
     </div>
   );
